331790917ef74613512818c12f9a7b21ea150314
bobriska <61400981+bobriska@users.noreply.github.com>
2022-11-14 20:40:54 +0200

    2.13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const FilterForm = ({filter, setFilter, countries, setCountries}) => {
-  const handleFilterChange = (event) => setFilter(event.target.value)
+const FilterForm = ({filter, setFilter, selectedCountry}) => {
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  //not quite sure why useEffect needed, but first click on show requires it 
+  useEffect(() => {if(selectedCountry.length > 0) {
+    setFilter(selectedCountry)
+  }}, [setFilter, selectedCountry])
   return(
     <form >
       <p>find countries: <input value={filter} onChange={handleFilterChange}/></p>
@@ -10,39 +16,45 @@ const FilterForm = ({filter, setFilter, countries, setCountries}) => {
   )
 }
 
-const Countries = ({filter, allCountries}) => {
-  const filteredCountries = allCountries
-    .filter(value => value.name.common.toLowerCase().includes(filter.toLowerCase()))
-    
+const Countries = ({filter, setCountry, allCountries}) => {
+  const filteredCountries = allCountries.filter(value => value.name.common.toLowerCase().includes(filter.toLowerCase()))
+  // if there is one country show that one
   if (filteredCountries.length === 1) { 
     return (
-      <ShowCountry selectedCountry={filteredCountries}/>
+      <ShowCountry countryToShow={filteredCountries}/>
     )
-  } else {
+  } 
+  // otherwise show list of countries
+  return (
+      <ShowCountries filteredCountries={filteredCountries} setCountry={setCountry}/>
+  )
+}
+
+const Button = ({handleClick, selected}) => 
+  <button onClick={() => handleClick(selected)}>show</button>
+
+const ShowCountries = ({filteredCountries, setCountry}) => {
+  //show list of countries if ten or less found
+  if (filteredCountries.length < 11) {
     return (
-      <ShowCountries filteredCountries={filteredCountries}/>
+      <div>{filteredCountries.map(value => 
+          <p key={value.name.common}>{value.name.common} 
+            <Button handleClick={setCountry} selected={value.name.common}/>
+          </p>)}
+      </div>
     )
   }
   
+  return (
+    <div> Too many matches, specify another filter </div>
+  )
 }
 
-const ShowCountries = ({filteredCountries}) => {
-  if (filteredCountries.length < 9) {
-    return (
-      <div>{filteredCountries.map(value => <p key={value.name.common}>{value.name.common} </p>)}</div>
-    )
-  } else {
-    return (
-      <div> Too many matches, specify another filter </div>
-    )
-  }
-}
-
-const ShowCountry = ({selectedCountry}) => {
-return (
-  <div>{selectedCountry
-  .map(value => 
-    <div key={value.name.common}>
+const ShowCountry = ({countryToShow}) => {
+  return (
+    <div>{countryToShow
+    .map(value => 
+      <div key={value.name.common}>
       <h2>{value.name.common} </h2>
       <p>capital {value.capital}</p> 
       <p>area {value.area}</p>
@@ -56,6 +68,7 @@ return (
 const App = () => {
   const [filter, setFilter] = useState('')
   const [allCountries, setAllCountries] = useState([])
+  const [selectedCountry, setCountry] = useState('')
 
   useEffect(() => {
       axios
@@ -67,8 +80,14 @@ const App = () => {
 
   return (
     < >
-      <FilterForm filter={filter} setFilter={setFilter}/>
-      <Countries filter={filter} allCountries={allCountries}/>
+      <FilterForm 
+        filter={filter} 
+        setFilter={setFilter} 
+        selectedCountry={selectedCountry}/>
+      <Countries 
+        filter={filter} 
+        setCountry={setCountry}
+        allCountries={allCountries}/>
     </>
   )
 }
